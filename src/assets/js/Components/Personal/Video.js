88ce135a07b7192ef857954d4cc1acb735729d93
Abhay pai <Abhay.Pai@bayviewtechnology.com>
2019-08-28 14:23:02 +0800

    Added custom control in video player

--- src/assets/js/Components/Personal/Video.js
+++ src/assets/js/Components/Personal/Video.js
@@ -8,11 +8,18 @@ import PropTypes from 'prop-types';
  *  Extending React Component
  */
 class Video extends React.Component {
+    constructor(props) {
+        super(props);
+        this.clickHandler = this.clickHandler.bind(this);
+        this.contextMenuHandler = this.contextMenuHandler.bind(this);
+    }
+
     render() {
         return(
             <div className='row'>
                 <div className='col-12 col-sm-12'>
-                    <video width='100%' controls>
+                    <video width='100%' onClick={this.clickHandler}
+                        onContextMenu={this.contextMenuHandler}>
                         <source src={this.props.url}
                             type='video/mp4' crossOrigin='anonymous' />
                         Your browser does not support HTML5 video.
@@ -20,6 +27,15 @@ class Video extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    contextMenuHandler(event) {
+        event.preventDefault();
+    }
+
+    clickHandler(event) {
+        event.preventDefault();
+        event.target.paused ? event.target.play() : event.target.pause();
     }
 }
 
